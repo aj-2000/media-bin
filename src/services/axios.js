cda7031792bb27aab2bf20076b9d3c7248923fe8
aj2000 <hi@ajaysharma.dev>
2023-03-27 21:40:45 +0530

    removed axios timeout

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,5 +2,4 @@ import axios from "axios";
 export const client = axios.create({
   baseURL: "https://convin-json-production.up.railway.app/",
   // baseURL: "http://localhost:8000",
-  timeout: 1000,
 });
