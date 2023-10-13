7a8dd206d816777ab12df268a84943804d14ee39
Ajay Sharma <97158497+aj-2000@users.noreply.github.com>
2023-10-13 21:30:34 +0530

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const client = axios.create({
-  baseURL: "https://convin-json-production.up.railway.app/",
+  baseURL: "https://convin-json.up.railway.app/",
   // baseURL: "http://localhost:8000",
 });
